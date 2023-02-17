757a926c3bb4e92864ae9686ff0fca1b6a60a797
Ruchitachaudhari16 <ruchitachaudhari70@gmail.com>
2023-02-16 19:01:09 -0500

    Employee_wages added

--- EmployeeWages/EmployeeWages/Program.cs
+++ EmployeeWages/EmployeeWages/Program.cs
@@ -5,16 +5,22 @@
         static void Main(string[] args)
         {
             int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
+            //variables
+            int emp_hrs = 0;
+            int emp_wage = 0;
             Random random = new Random();
-            int empCheck = random.Next(0, 2);
+            int empCheck = random.Next(0,2);
             if (empCheck == IS_FULL_TIME)
             {
-                Console.WriteLine("Employee is Present");
+                emp_hrs = 8;
             }
             else
             {
-                Console.WriteLine("Employee is Abscent");
+                emp_hrs = 0;
             }
+            emp_wage = emp_hrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Employee Wage Is :" + emp_wage);
 
         }
     }
