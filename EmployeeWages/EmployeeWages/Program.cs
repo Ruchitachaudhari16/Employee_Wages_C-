8321b8c0732ebd2980240fd5faa509dcbeab3423
Ruchitachaudhari16 <ruchitachaudhari70@gmail.com>
2023-02-16 19:35:13 -0500

    Employee wages addded using a switch case statement

--- EmployeeWages/EmployeeWages/Program.cs
+++ EmployeeWages/EmployeeWages/Program.cs
@@ -2,28 +2,30 @@
 {
     internal class Program
     {
+       const int IS_PART_TIME = 1;
+        const int IS_FULL_TIME = 2;
+         const int EMP_RATE_PER_HOUR = 20;
         static void Main(string[] args)
         {
-            int IS_PART_TIME = 1;
-            int IS_FULL_TIME = 2;
-            int EMP_RATE_PER_HOUR = 20;
+           
             //variables
             int emp_hrs = 0;
             int emp_wage = 0;
             Random random = new Random();
             int empCheck = random.Next(0,3);
-            if (empCheck == IS_PART_TIME)
+            switch (empCheck)
             {
-                emp_hrs = 4;
-            }
-            else if (empCheck == IS_FULL_TIME)
-            {
-                emp_hrs = 8;
-            }
-            else 
-            {
-                emp_hrs = 0;
-            }
+                case IS_PART_TIME:
+                    emp_hrs = 4;
+                    break;
+                case IS_FULL_TIME:
+                    emp_hrs = 8;
+                    break;
+                default:
+                    emp_hrs = 0;
+                    break;
+            
+        }
             emp_wage = emp_hrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Employee Wage Is :" + emp_wage);
 
