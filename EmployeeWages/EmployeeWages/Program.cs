d48df6ed470013cf7d0275c313cce030ddb019da
Ruchitachaudhari16 <ruchitachaudhari70@gmail.com>
2023-02-16 19:17:00 -0500

    Part time WAges is Calculated

--- EmployeeWages/EmployeeWages/Program.cs
+++ EmployeeWages/EmployeeWages/Program.cs
@@ -4,18 +4,23 @@
     {
         static void Main(string[] args)
         {
-            int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 1;
+            int IS_FULL_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
             //variables
             int emp_hrs = 0;
             int emp_wage = 0;
             Random random = new Random();
-            int empCheck = random.Next(0,2);
-            if (empCheck == IS_FULL_TIME)
+            int empCheck = random.Next(0,3);
+            if (empCheck == IS_PART_TIME)
+            {
+                emp_hrs = 4;
+            }
+            else if (empCheck == IS_FULL_TIME)
             {
                 emp_hrs = 8;
             }
-            else
+            else 
             {
                 emp_hrs = 0;
             }
